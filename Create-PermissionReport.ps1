8e956c3231abd7c847ea7dfd6f422db167195d7d
Kyle MacKenzie <elykcam@gmail.com>
2021-10-03 21:54:39 -0300

    Update Create-PermissionReport.ps1

--- Create-PermissionReport.ps1
+++ Create-PermissionReport.ps1
@@ -4,28 +4,28 @@
 
 param(
     [Parameter(Mandatory=$False,HelpMessage="Specifies whether or not to perform a test run (do not create file)")]
-    [boolean]$TestRun,
+    [boolean]$TestRun = $False,
 
     [Parameter(Mandatory=$False,HelpMessage="The root directory to start reading permissions")]
-    [boolean]$Path,
+    [string]$Path = '.',
 
-    [Parameter(Mandatory=$False,HelpMessage="Whether or not to traverse subfolders in root directory")]
-    [boolean]$TraverseSubFolders,
+    [Parameter(Mandatory=$False,HelpMessage="Whether or not to traverse subfolders in root directory (Default:False)")]
+    [boolean]$TraverseSubFolders = $False,
 
-    [Parameter(Mandatory=$False,HelpMessage="Read Active Directory for additional permissions from groups")]
-    [boolean]$ExpandADPermissions,
+    [Parameter(Mandatory=$False,HelpMessage="Read Active Directory for additional permissions from groups (Default:False)")]
+    [boolean]$ExpandADPermissions = $True,
 
-    [Parameter(Mandatory=$False,HelpMessage="Output file permissions. True by default")]
-    [boolean]$CheckFiles,
+    [Parameter(Mandatory=$False,HelpMessage="Output file permissions (Default:True)")]
+    [boolean]$CheckFiles = $True,
 
-    [Parameter(Mandatory=$False,HelpMessage="Output folder permissions. True by default")]
-    [boolean]$CheckFolders
+    [Parameter(Mandatory=$False,HelpMessage="Output folder permissions (Default:True)")]
+    [boolean]$CheckFolders = $True
 )
 
 
-*************************************************************************************
+#*************************************************************************************
 #                         DEFINE CLASS CONSTRUCTORS
-*************************************************************************************
+#*************************************************************************************
 
 #Creates a new custom defined 'User' object
 function NewUser(){
@@ -69,18 +69,48 @@ function NewDirectory(){
 
 }
 
-*************************************************************************************
+#*************************************************************************************
 #                         DEFINE HELPER METHODS
-*************************************************************************************
+#*************************************************************************************
+
+function logR($msg){ Write-Host $msg -ForegroundColor Red }
+function logG($msg){ Write-Host $msg -ForegroundColor Green }
+function logY($msg){ Write-Host $msg -ForegroundColor Yellow }
 
 
 
+#*************************************************************************************
+#                         INITALATION / VALIDATION
+#*************************************************************************************
 
-*************************************************************************************
+$_RootDir = '';
+
+#Check that there is a valid directory
+#If the path is default, use the current directory
+if( $path -EQ '.' ){
+    $_RootDir = Get-Location;
+
+#If the path was passed, only continue if it is valid
+} else {
+    if( [System.IO.Directory]::Exists($path) ){
+        $_RootDir = $Path;
+    } else {
+        throw "Cannot find specified path"
+    }
+}
+
+
+
+#*************************************************************************************
 #                         TEST CODE
-*************************************************************************************
+#*************************************************************************************
+
+#Check root directory
+logY("The root directory is ["+$_RootDir+"]")
+
+#Read all entries from the root directory
 
 
-*************************************************************************************
+#*************************************************************************************
 #                         BEGIN PROCESSING
-*************************************************************************************
+#*************************************************************************************
